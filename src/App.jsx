a24dfe9a3ffe6a99d3c45bb668f0d0ffd728c14d
Mohammed Islam <saeedaj357@gmail.com>
2025-07-02 12:38:30 -0400

    Bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,24 @@ import { createRoot } from "react-dom/client";
 import "./AppStyles.css";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Routes } from "react-router";
+import AllCampuses from "./components/AllCampuses";
+import AllStudents from "./components/AllStudents";
+import SingleStudent from "./components/SingleStudent";
+import SingleCampus from "./components/SingleCampus";
 
 const App = () => {
   return (
-    <div>
-      <NavBar />
-      <div className="app">
-        <h1>Hello React!</h1>
-        <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
-
-        <Routes>{/* Currently, we don't have any routes defined */}</Routes>
-      </div>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/campuses" element={<AllCampuses />} />
+        <Route path="/students" element={<AllStudents />} />
+        <Route path="/campuses/:id" element={<SingleCampus />} />
+        <Route path="/students/:id" element={<SingleStudent />} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </Router>
   );
 };
 
-// We're using React Router to handle the navigation between pages.
-// It's important that the Router is at the top level of our app,
-// and that we wrap our entire app in it. With this in place, we can
-// declare Routes, Links, and use useful hooks like useNavigate.
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <Router>
-    <App />
-  </Router>
-);
+export default App;
---
+++ src/components/SingleStudent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SingleStudent = () => {
+  return (
+    <div>
+      <h2>Student View</h2>
+      <p>Student details.</p>
+    </div>
+  );
+};
+
+export default SingleStudent;
