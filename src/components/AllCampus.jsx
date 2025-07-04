512c5d31622ae03e43a1376450466442c2f59957
roselyn5554 <r.marte445@gmail.com>
2025-07-04 12:33:25 -0400

    adding css to the color

--- src/components/AllCampus.jsx
+++ src/components/AllCampus.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
 import axios from "axios";
 
-<<<<<<< HEAD
 
   const AllCampus = () => {
   const [campus, setCampus] = useState([]);
@@ -11,16 +10,6 @@ import axios from "axios";
     axios.get("http://localhost:8080/api/campus")
       .then((res) => setCampuses(res.data))
       .catch((err) => console.error("Error fetching campuses:", err));
-=======
-const AllCampus = () => {
-  const [campus, setCampus] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/api/campus")
-      .then((res) => setCampus(res.data))
-      .catch((err) => console.error("Error fetching campus:", err));
->>>>>>> cffaea0243b9bd52014485278aaffb51a66729b2
   }, []);
 
   if (campus.length === 0) {
