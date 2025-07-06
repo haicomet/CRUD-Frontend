cb49823b6e9fc02a0f1480dd38d37e3b3262167c
hailia sommerville <hailiassommerville@gmail.com>
2025-07-06 14:02:17 -0400

    changed import to react-router-dom

--- src/components/AllStudents.jsx
+++ src/components/AllStudents.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import DeleteStudent from "./DeleteStudent";
 import StudentCard from "./StudentCard";
