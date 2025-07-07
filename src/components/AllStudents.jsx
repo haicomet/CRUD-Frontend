41d6fe7445c4e5e3abc617a37039c49c53d44239
roselyn5554 <r.marte445@gmail.com>
2025-07-06 22:57:24 -0400

    designed buttons

--- src/components/AllStudents.jsx
+++ src/components/AllStudents.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import DeleteStudent from "./DeleteStudent";
 import StudentCard from "./StudentCard";
 const API_URL = process.env.API_URL || "http://localhost:8080";
+import "./styles/buttons.css";
 
 
 const AllStudents = () => {
