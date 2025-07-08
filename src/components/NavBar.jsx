e9545effe94111772f2a79bd3f9fa96f6e757ef0
roselyn5554 <r.marte445@gmail.com>
2025-07-07 23:10:16 -0400

    trying

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,21 +2,31 @@ import React from "react";
 import { Link } from "react-router";
 import "./styles/Navbar.css";
 
-
 const Navbar = () => {
   return (
     <nav style={{ padding: "1rem", background: "#f0f0f0" }}>
-
-      <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
-      <Link to="/campus" style={{ marginRight: "1rem" }}>Campus</Link>
-      <Link to="/students" style={{ marginRight: "1rem" }}>Students</Link>
-      <Link to="/add-student" style={{ marginRight: "1rem" }}>Add Student</Link>
-      <Link to="/add-campus" style={{ marginRight: "1rem" }} >Add Campus</Link>
-<Link to="/add-student" style={{ marginRight: "1rem" }}>Sign in</Link>
-      <Link to="/add-campus" style={{ marginRight: "1rem" }} >Sign Up</Link>
-
+      <Link to="/" style={{ marginRight: "1rem" }}>
+        Home
+      </Link>
+      <Link to="/campus" style={{ marginRight: "1rem" }}>
+        Campus
+      </Link>
+      <Link to="/students" style={{ marginRight: "1rem" }}>
+        Students
+      </Link>
+      <Link to="/add-student" style={{ marginRight: "1rem" }}>
+        Add Student
+      </Link>
+      <Link to="/add-campus" style={{ marginRight: "1rem" }}>
+        Add Campus
+      </Link>
+      <Link to="/add-student" style={{ marginRight: "1rem" }}>
+        Sign in
+      </Link>
+      <Link to="/add-campus" style={{ marginRight: "1rem" }}>
+        Sign Up
+      </Link>
     </nav>
-    
   );
 };
 
